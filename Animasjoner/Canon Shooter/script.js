050e7db64e3d9d4b411bc1919e1ff673fd53f594
CHLS28 <christian.kramara@gmail.com>
2023-11-28 11:21:35 +0100

    Canon shooter

---
+++ Animasjoner/Canon Shooter/script.js
@@ -0,0 +1,44 @@
+function sjekkKollisjon(){
+
+}
+
+function flyttKula(){
+    if (!erILufta){
+        // Stopp animasjon når kula er utenfor skjermen
+        skytBtn.disabled = false;
+        return;
+    }
+    xpos += xfart;
+
+    kule.style.left = xpos + "px";
+
+    sjekkKollisjon();
+
+    if (xpos > window.innerWidth + 50){
+        // Flytt kula bak kanonen
+        xpos = 250; 
+        kule.style.left = xpos + "px";
+        erILufta = false;
+
+    }
+    requestAnimationFrame(flyttKula);
+}
+
+function skyt(){
+    erILufta = true;
+    skytBtn.disabled = true;
+    flyttKula();
+
+}
+let xpos = 250;
+let ypos = 440;
+let xfart = 10;
+let radiusK = 17.5;
+let erILufta = false;
+
+// Fiendekule
+let xposF = 750;
+let yposF = 440;
+let radiusF = 50;
+
+skytBtn.addEventListener("click", skyt);
