27ad0d147f99d19210bfbf6911542fdf1ef3ad0d
CHLS28 <christian.kramara@email.com>
2024-04-25 09:59:48 +0200

    hei

--- Finalen/Kunstgalleri/Public/script.js
+++ Finalen/Kunstgalleri/Public/script.js
@@ -1,21 +1,17 @@
-function toggleImageSize(image) {
-    if (image.classList.contains('enlarge')) {
-        image.classList.remove('enlarge');
-    } else {
-        image.classList.add('enlarge');
-    }
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-    const images = document.querySelectorAll(".galleri img");
-
-    images.forEach(img => {
-        img.addEventListener("click", function() {
-            if (this.classList.contains("enlarge")) {
-                this.classList.remove("enlarge");
-            } else {
-                this.classList.add("enlarge");
+document.addEventListener('DOMContentLoaded', function() {
+    const images = document.querySelectorAll('.galleri img');
+    const blurOverlay = document.getElementById('blurOverlay');
+    images.forEach(image => {
+      image.addEventListener('click', function() {
+        images.forEach(image => {
+            if (image != this){
+                image.classList.remove('zoomed');
             }
-        });
+        })
+        this.classList.toggle('zoomed'); // Toggle active class on click
+        this.classList.toggle('zoom-out');
+        blurOverlay.classList.toggle('blur'); 
+        // blurOverlay.style.opacity = 1;
+      });
     });
-});
+    });
