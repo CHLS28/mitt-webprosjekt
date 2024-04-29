90a5c320d074c59a30a5522755fc78dafc431de9
CHLS28 <christian.kramara@email.com>
2024-04-29 15:40:13 +0200

    yee

---
+++ EJS_og_node/4.2 Passing Data til elever/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  let fNavn = req.body.fName;
+  let eNavn = req.body.lName;
+  
+  let lengde = fNavn.length + eNavn.length;
+  res.render("index.ejs",
+  {navnLengde: lengde});
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
